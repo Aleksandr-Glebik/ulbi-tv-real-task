f8b15c21b7da4163f2c935ec8ebda3148be7ea0b
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-16 14:57:15 +0300

    feat: add func isDateAfter in rules; add new rules for datePicker

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,8 +1,18 @@
+import moment, { Moment } from 'moment';
+import { message } from "antd";
 
 
 export const rules = {
     required: (message: string = 'Обязательное поле') => ({
       required: true,
       message
+    }),
+    isDateAfter: (message: string) => () => ({
+      validator(_: any, value: Moment) {
+        if(value.isSameOrAfter(moment())) {
+          return Promise.resolve()
+        }
+        return Promise.reject(new Error(message))
+      }
     })
 }
